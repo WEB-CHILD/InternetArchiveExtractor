03328441fbb6b837daf1cec77953d67675dcd7da
Victor Johnston <vijo@cas.au.dk>
2025-10-02 08:42:18 +0200

    Add functionality for downloading files from Internet Archive and do some cleanup

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 def read_csv(file_path):
     import pandas as pd
-    return pd.read_csv(file_path)
+    return pd.read_csv(file_path, sep=";")
 
 def remove_port_80(url):
     if ':80' in url:
@@ -22,3 +22,24 @@ def create_warc_gz(file_path, dataframe):
         for index, row in dataframe.iterrows():
             writer.write_webpage(row['url_origin'], row['timestamp'], content_type='text/html')
             writer.write_webpage(row['url_archive'], row['timestamp'], content_type='text/html')
+
+def import_urls_from_csv(file_path, column_name):
+    """
+    Imports URLs from a specified column in a CSV file.
+
+    Args:
+        file_path (str): The path to the CSV file.
+        column_name (str): The name of the column containing URLs.
+
+    Returns:
+        list: A list of URLs extracted from the specified column.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        KeyError: If the specified column does not exist in the CSV file.
+        pd.errors.EmptyDataError: If the CSV file is empty.
+    """
+
+    df = read_csv(file_path)
+    url_list = df[column_name].tolist()
+    return url_list
